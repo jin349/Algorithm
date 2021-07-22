a5675d00a130d6d851c6a5242c362a367c9991c1
jin349 <85613263+jin349@users.noreply.github.com>
2021-07-23 01:49:41 +0900

    Add files via upload

---
+++ June30/Study01.py
@@ -0,0 +1,14 @@
+
+# for i in range(1,6):
+#     for j in range(1,10):
+#         print(i, "*", j, "=", i*j)
+
+a = [[1,2], [3,4], [5,6]]
+for i in range(len(a)):
+
+    for j in range(len(a[i])):
+        print(a[i][j])
+
+# for i in range(1, 11):
+#     print(i)
+
